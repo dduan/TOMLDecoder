58de07a2f79c6784c6969f1551fdebfa68512a83
giginet <giginet.net@gmail.com>
2019-06-26 04:43:34 +0900

    Avoid warnings

--- Sources/TOMLDecoder/TOMLDecoder.swift
+++ Sources/TOMLDecoder/TOMLDecoder.swift
@@ -1200,7 +1200,7 @@ extension TOMLDecoder.KeyDecodingStrategy {
         let leadingUnderscoreRange = stringKey.startIndex..<firstNonUnderscore
         let trailingUnderscoreRange = stringKey.index(after: lastNonUnderscore)..<stringKey.endIndex
 
-        var components = stringKey[keyRange].split(separator: "_")
+        let components = stringKey[keyRange].split(separator: "_")
         let joinedString : String
         if components.count == 1 {
             // No underscores in key, leave the word as is - maybe already camel cased
