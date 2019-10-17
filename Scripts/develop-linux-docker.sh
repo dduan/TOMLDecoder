7759699e6b7d198b70503675921e0fb7f0283b86
Daniel Duan <daniel@duan.ca>
2019-10-16 23:57:06 -0700

    Simplify Docker setup

--- Scripts/develop-linux-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-command -v docker &> /dev/null || { echo >&2 "Install docker https://www.docker.com"; exit 1; }
-
-IMAGE=norionomura/swift@sha256:3455f611208d110f04c2420717bf2a5cf1124b23c91690fee1bdc278b8838491
-NAME=tomldeserializerdev
-docker run -it -v "$PWD":/TOMLDecoder --name "$NAME" --rm "$IMAGE"
