bb6358c0e09c7aa267f8ee819c4df297d6a1dd1a
Daniel Duan <daniel@duan.ca>
2021-01-02 15:40:17 -0800

    Break source into multiple files

---
+++ Sources/TOMLDecoder/Decoding/TOMLDecodingStorage.swift
@@ -0,0 +1,33 @@
+struct TOMLDecodingStorage {
+    // MARK: Properties
+
+    /// The container stack.
+    /// Elements may be any one of the JSON types (NSNull, NSNumber, String, Array, [String : Any]).
+    private(set) var containers: [Any] = []
+
+    // MARK: - Initialization
+
+    /// Initializes `self` with no containers.
+    init() {}
+
+    // MARK: - Modifying the Stack
+
+    var count: Int {
+        return self.containers.count
+    }
+
+    var topContainer: Any {
+        precondition(!self.containers.isEmpty, "Empty container stack.")
+        return self.containers.last!
+    }
+
+    mutating func push(container: Any) {
+        self.containers.append(container)
+    }
+
+    mutating func popContainer() {
+        precondition(!self.containers.isEmpty, "Empty container stack.")
+        self.containers.removeLast()
+    }
+}
+
