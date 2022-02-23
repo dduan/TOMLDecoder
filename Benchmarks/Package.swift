95871e223aa6c1b485a013bcb3c8484e85c3573e
Daniel Duan <daniel@duan.ca>
2022-02-23 13:07:07 -0800

    Update benchmark library

--- Benchmarks/Package.swift
+++ Benchmarks/Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "Benchmarks",
     dependencies: [
         .package(name: "TOMLDecoder", path: "../"),
-        .package(name: "Benchmark", url: "https://github.com/google/swift-benchmark", .exact("0.1.0")),
+        .package(name: "Benchmark", url: "https://github.com/google/swift-benchmark", .exact("0.1.1")),
         .package(name: "TOMLDeserializer", url: "https://github.com/dduan/TOMLDeserializer", .exact("0.2.5")),
     ],
     targets: [
