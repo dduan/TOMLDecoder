52e918d16b957ba2ddf65949b31d487179b74ffd
Larry Zeng <hi@universe.observer>
2023-04-16 11:10:40 -0700

    Add compliance module for toml-test

--- Package.swift
+++ Package.swift
@@ -6,11 +6,13 @@ let package = Package(
     name: "TOMLDecoder",
     platforms: [.iOS(.v9), .tvOS(.v9), .watchOS(.v2), .macOS(.v10_10)],
     products: [
+        .executable(name: "compliance", targets: ["compliance"]),
         .library(
             name: "TOMLDecoder",
             targets: ["TOMLDecoder"]),
     ],
     targets: [
+        .target(name: "compliance", dependencies: ["TOMLDecoder"]),
         .target(
             name: "TOMLDecoder",
             dependencies: ["Deserializer"]),
