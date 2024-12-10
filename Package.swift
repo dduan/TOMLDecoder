6bf08b3b4b072cd53008ad49674bc1e58d23aa3f
Daniel Duan <daniel@duan.ca>
2024-12-10 08:58:34 -0800

    Make changes for newer Swift version

--- Package.swift
+++ Package.swift
@@ -1,31 +1,26 @@
-// swift-tools-version:5.0
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let package = Package(
     name: "TOMLDecoder",
-    platforms: [.iOS(.v9), .tvOS(.v9), .watchOS(.v2), .macOS(.v10_10)],
+    platforms: [.iOS(.v12), .tvOS(.v12), .watchOS(.v4), .macOS(.v13)],
     products: [
         .executable(name: "compliance", targets: ["compliance"]),
-        .library(
-            name: "TOMLDecoder",
-            targets: ["TOMLDecoder"]),
+        .library(name: "TOMLDecoder", targets: ["TOMLDecoder"]),
     ],
     targets: [
-        .target(name: "compliance", dependencies: ["TOMLDecoder"]),
-        .target(
-            name: "TOMLDecoder",
-            dependencies: ["Deserializer"]),
+        .executableTarget(name: "compliance", dependencies: ["TOMLDecoder"]),
+        .target(name: "TOMLDecoder", dependencies: ["Deserializer"]),
         .target(name: "Deserializer"),
-        .testTarget(
-            name: "TOMLDecoderTests",
-            dependencies: ["TOMLDecoder"]),
+        .testTarget(name: "TOMLDecoderTests", dependencies: ["TOMLDecoder"]),
         .testTarget(
             name: "DeserializerTests",
             dependencies: ["Deserializer"],
             exclude: [
                 "invalid_fixtures",
                 "valid_fixtures",
-            ]),
+            ]
+        ),
     ]
 )
