5090880b0ecde941a5ed5cbc5cf95e362cb93e69
Daniel Duan <daniel@duan.ca>
2025-11-14 10:56:16 -0800

    Remove warning in tests

--- Tests/TOMLDecoderTests/TOMLDecoderTests.swift
+++ Tests/TOMLDecoderTests/TOMLDecoderTests.swift
@@ -332,6 +332,6 @@ final class TOMLDecoderTests: XCTestCase {
         """
 
         let decoder = TOMLDecoder()
-        let result = try decoder.decode(LocalPlayer.self, from: toml)
+        _ = try decoder.decode(LocalPlayer.self, from: toml)
     }
 }
