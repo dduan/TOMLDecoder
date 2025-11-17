e3e6756f7ac1148d1678d49ae0ca253808250b6e
Daniel Duan <daniel@duan.ca>
2025-11-17 01:15:58 -0800

    Undo some

--- Package.swift
+++ Package.swift
@@ -1,17 +1,6 @@
 // swift-tools-version: 6.1
 
 import PackageDescription
-import class Foundation.ProcessInfo
-
-var dependencies: [Package.Dependency] = []
-let env = ProcessInfo.processInfo.environment
-let useExperimental = env["BUILD_DOCS"] == "1"
-
-if useExperimental {
-    dependencies.append(
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
-    )
-}
 
 let package = Package(
     name: "TOMLDecoder",
@@ -20,10 +9,6 @@ let package = Package(
         .executable(name: "compliance", targets: ["compliance"]),
         .library(name: "TOMLDecoder", targets: ["TOMLDecoder"]),
     ],
-    traits: [
-        "docs",
-    ],
-    dependencies: dependencies,
     targets: [
         .executableTarget(name: "compliance", dependencies: ["TOMLDecoder"]),
         .target(
