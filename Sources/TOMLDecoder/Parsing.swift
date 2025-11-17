206044fd53d412d5147c44caac21fe39cf334720
Daniel Duan <daniel@duan.ca>
2025-11-16 23:30:08 -0800

    Remove unused variables and function (#113)
    * Remove unused variables
    
    * fix one more
    
    * remove

--- Sources/TOMLDecoder/Parsing.swift
+++ Sources/TOMLDecoder/Parsing.swift
@@ -75,7 +75,6 @@ extension UTF8.CodeUnit {
 
 struct Token {
     enum Kind {
-        case invalid
         case dot
         case comma
         case equal
@@ -276,7 +275,6 @@ struct Deserializer {
 
     let originalSource: String
     let sourceUTF8: String.UTF8View
-    var errors: [String]
     var token: Token
     var currentTable: Int
     var tablePath: [(String, Token)] = []
@@ -286,7 +284,6 @@ struct Deserializer {
         self.tables = [TOMLTableImplementation()]
         self.originalSource = source
         self.sourceUTF8 = source.utf8
-        self.errors = []
         self.token = Token(kind: .newline, lineNumber: 1, text: "".utf8[...], eof: false)
         self.currentTable = 0
         self.keyTransform = keyTransform
@@ -989,26 +986,6 @@ func parseTimezoneOffset(_ text: String.UTF8View.SubSequence, lineNumber: Int) t
     consumedLength += 2
 
     return (offsetHour, offsetMinute, consumedLength)
-}
-
-func toTimestamp(
-    year: Int, month: Int, day: Int,
-    hour: Int, minute: Int, seconds: Int,
-    nanoseconds: Int, offsetInSeconds: Int
-) -> Double {
-    var y = year
-    if month <= 2 { y -= 1 }
-
-    let era = (y >= 0 ? y : y - 399) / 400
-    let yoe = y - era * 400
-    let doy = (153 * (month + (month > 2 ? -3 : 9)) + 2) / 5 + day - 1
-    let doe = yoe * 365 + yoe / 4 - yoe / 100 + doy
-    let epochOffset = 719_468
-    let dayCounts = Int64(era) * 146_097 + Int64(doe) - Int64(epochOffset)
-    let secondOfDay = Int64(hour) * 3_600 + Int64(minute) * 60 + Int64(seconds)
-    let totalSeconds = dayCounts * 86_400 + secondOfDay - Int64(offsetInSeconds)
-
-    return Double(totalSeconds) + Double(nanoseconds) / 1_000_000_000
 }
 
 extension Deserializer {
