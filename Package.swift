198c195b2be18d2dc9cf1d83cf91a864a3ce9051
Daniel Duan <daniel@duan.ca>
2025-11-17 23:21:48 -0800

    Modularize Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,61 @@
 // swift-tools-version: 6.1
 
 import PackageDescription
+import Foundation
+
+let includeBenchmarks = ProcessInfo.processInfo.environment["TOMLDECODER_BENCHMARKS"] == "1"
+
+var dependencies: [Package.Dependency] = includeBenchmarks ? [
+    .package(
+        url: "https://github.com/ordo-one/package-benchmark",
+        .upToNextMajor(from: "1.4.0")
+    ),
+] : []
+
+var targets: [Target] = [
+    .executableTarget(
+        name: "compliance",
+        dependencies: ["TOMLDecoder"]
+    ),
+    .target(
+        name: "TOMLDecoder",
+        exclude: [
+            "gyb",
+        ]
+    ),
+]
+
+var testTargets: [Target] = [
+    .target(
+        name: "ProlepticGregorianTestHelpers",
+        publicHeadersPath: "include"
+    ),
+    .testTarget(
+        name: "TOMLDecoderTests",
+        dependencies: [
+            "TOMLDecoder",
+            "ProlepticGregorianTestHelpers",
+        ],
+        exclude: [
+            "invalid_fixtures",
+            "valid_fixtures",
+        ]
+    ),
+]
+
+var benchmarkTargets: [Target] = includeBenchmarks ? [
+    .executableTarget(
+        name: "TOMLDecoderBenchmarks",
+        dependencies: [
+            "TOMLDecoder",
+            .product(name: "Benchmark", package: "package-benchmark"),
+        ],
+        path: "Benchmarks/TOMLDecoderBenchmarks",
+        plugins: [
+            .plugin(name: "BenchmarkPlugin", package: "package-benchmark"),
+        ]
+    ),
+] : []
 
 let package = Package(
     name: "TOMLDecoder",
@@ -9,49 +64,7 @@ let package = Package(
         .executable(name: "compliance", targets: ["compliance"]),
         .library(name: "TOMLDecoder", targets: ["TOMLDecoder"]),
     ],
-    dependencies: [
-        .package(
-            url: "https://github.com/ordo-one/package-benchmark",
-            .upToNextMajor(from: "1.4.0")
-        ),
-    ],
-    targets: [
-        .executableTarget(
-            name: "compliance",
-            dependencies: ["TOMLDecoder"]
-        ),
-        .target(
-            name: "TOMLDecoder",
-            exclude: [
-                "gyb",
-            ]
-        ),
-        .target(
-            name: "ProlepticGregorianTestHelpers",
-            publicHeadersPath: "include"
-        ),
-        .testTarget(
-            name: "TOMLDecoderTests",
-            dependencies: [
-                "TOMLDecoder",
-                "ProlepticGregorianTestHelpers",
-            ],
-            exclude: [
-                "invalid_fixtures",
-                "valid_fixtures",
-            ]
-        ),
-        .executableTarget(
-            name: "TOMLDecoderBenchmarks",
-            dependencies: [
-                "TOMLDecoder",
-                .product(name: "Benchmark", package: "package-benchmark"),
-            ],
-            path: "Benchmarks/TOMLDecoderBenchmarks",
-            plugins: [
-                .plugin(name: "BenchmarkPlugin", package: "package-benchmark"),
-            ]
-        ),
-    ],
+    dependencies: dependencies,
+    targets: targets + testTargets + benchmarkTargets,
     cxxLanguageStandard: .cxx20
 )
